08f4041b6a3cf377f3514610762e121cfdd20e55
herewegolang <herewegolang@gmail.com>
2019-03-19 23:28:11 +0900

    ./

---
+++ 02_Variable.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	// 1. var 변수명 자료형
+	var i int
+	var s string
+
+	// 2. var 변수형 자료형 = 초기값
+	var year int = 2019
+	var name string = "Golang"
+
+	// 3. 짧은 선언 -> 변수명 := 초값
+	i := 1
+	s := "String"
+
+	// 4. 다수의 변수 선언
+	// var 변수1, 변수2 자료형 = 초기값1, 초기값2
+	var x, y int = 1, 2
+	// var 변수1, 변수2 = 초기값1, 초기값2
+	var FirstName, LastName = "Go", "Lang"
+	// 변수 1, 변수2 := 초기값1, 초기값2
+	a0, a1, a2, a3 := true, two, 3.001, 4
+
+}
